2b6f25ced54554e91f4ada8079e62379507edec8
Droissart Nicolas <nicodrg@live.com>
2021-02-07 23:41:37 +0100

    add 404 page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./NotFound.css";
 
 const NotFound = () => (
-    <div className="">
-        ! --- 404 Not Found --- !
+    <div id="main">
+        <div className="fof">
+            <h1>Erreur 404</h1>
+            <h5>La page que vous cherchez... n'existe pas (ou plus).</h5>
+        </div>
     </div>
 );
 
